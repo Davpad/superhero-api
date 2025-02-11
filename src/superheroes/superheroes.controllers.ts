c1d7e7015b98c03f7d2e76bad3e01172fb3ebb79
Davpad <davidpadillavillen@gmail.com>
2025-02-11 14:35:51 +0100

    Feat: add coments

--- src/superheroes/superheroes.controllers.ts
+++ src/superheroes/superheroes.controllers.ts
@@ -1,22 +1,21 @@
 import { Body, Controller, Get, Post} from "@nestjs/common";
 import { SuperheroesService } from "./superheroes.service";
 import { Superhero } from "./superheroes.service";
-import { log } from "console";
 
 @Controller('/superheroes')
 export class SuperheroesControllers{
 
-    constructor(private superheroesService:SuperheroesService){
+    constructor(private superheroesService:SuperheroesService){ //Calling the store and stablishing its type
     }
     @Get()
     getAllSuperheroes() {
-        return this.superheroesService.getSuperheroes();
+        return this.superheroesService.getSuperheroes(); //Calling the get function from superheroesService
     }
 
     @Post()
-    createSuperhero(@Body() superhero:Superhero) {
+    createSuperhero(@Body() superhero:Superhero) { //Stablishing the type of the body that is being posted
      
         
-        return this.superheroesService.createSuperhero(superhero);
+        return this.superheroesService.createSuperhero(superhero); //Calling the post function from superheroesService
     }
 }
--- src/superheroes/superheroes.service.ts
+++ src/superheroes/superheroes.service.ts
@@ -1,24 +1,24 @@
 import { Injectable } from "@nestjs/common";
 
-export interface Superhero {
+export interface Superhero { //The interface of the Superhero object, where I specify the types of the diferent elements.
     name: string;
     superpower: string;
     humility_score: number;
 }
 
 @Injectable()
-export class SuperheroesService{
+export class SuperheroesService{ //Inside this class are stored the functions and stores of the API
     
-    private superheroes : Superhero[] = []
+    private superheroes : Superhero[] = [] //All the superheroes are stored locally in this array
 
     getSuperheroes() {
-       return this.superheroes.sort((a, b)=> a.humility_score < b.humility_score? -1 : 1)
+       return this.superheroes.sort((a, b)=> a.humility_score < b.humility_score? -1 : 1) //I'm returning a sorted version of the superheroes array, in ascending order of the humility score.
     }
 
     createSuperhero(superhero: Superhero) {
         
         if (superhero.humility_score > 10){
-            return "Superheroes' humility score can't be higher than 10. Superheroes are still humans!"
+            return "Superheroes' humility score can't be higher than 10. Superheroes are still humans!" //This apply a conditional to evaluate if the humility score is higher than 10, if so it returns a message telling it can't be higher than 10.
         }
 
         this.superheroes.push(superhero)
