afc5970d351b4c77b48e05c884869b75b3c4a71d
Davpad <davidpadillavillen@gmail.com>
2025-02-11 11:37:12 +0100

    Feat: add logic to post method

--- src/superheroes/superheroes.service.ts
+++ src/superheroes/superheroes.service.ts
@@ -9,15 +9,19 @@ export interface Superhero {
 @Injectable()
 export class SuperheroesService{
     
+    private superheroes : Superhero[] = []
+
     getSuperheroes() {
     }
 
-    createSuperhero(superhero:Superhero) {
+    createSuperhero(superhero: Superhero) {
         
         if (superhero.humility_score > 10){
             return "Superheroes' humility score can't be higher than 10. Superheroes are still humans!"
         }
-        console.log(superhero);
+
+        this.superheroes.push(superhero)
+        return this.superheroes;
         
     }
 }
